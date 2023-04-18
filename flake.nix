4f58859a7a3732da996d9e9007e64f577e92dc7c
Felix <zen9.felix@gmail.com>
2023-04-18 23:11:54 +0800

    Inject current grpc gemconfig

--- flake.nix
+++ flake.nix
@@ -28,7 +28,36 @@
           inherit system;
           overlays = [ bob-ruby.overlays.default ];
         };
-        gemConfig = {};
+        gemConfig = with pkgs; {
+          grpc = attrs: {
+            nativeBuildInputs = [ pkg-config ]
+              ++ lib.optional stdenv.isDarwin cctools;
+            buildInputs = [ openssl ];
+            hardeningDisable = [ "format" ];
+            env.NIX_CFLAGS_COMPILE = toString [
+              "-Wno-error=stringop-overflow"
+              "-Wno-error=implicit-fallthrough"
+              "-Wno-error=sizeof-pointer-memaccess"
+              "-Wno-error=cast-function-type"
+              "-Wno-error=class-memaccess"
+              "-Wno-error=ignored-qualifiers"
+              "-Wno-error=tautological-compare"
+              "-Wno-error=stringop-truncation"
+            ];
+            dontBuild = false;
+            postPatch = ''
+              substituteInPlace Makefile \
+                --replace '-Wno-invalid-source-encoding' ""
+            '' + lib.optionalString stdenv.isDarwin ''
+              # For < v1.48.0
+              substituteInPlace src/ruby/ext/grpc/extconf.rb \
+                --replace "ENV['AR'] = 'libtool -o' if RUBY_PLATFORM =~ /darwin/" ""
+              # For >= v1.48.0
+              substituteInPlace src/ruby/ext/grpc/extconf.rb \
+                --replace 'apple_toolchain = ' 'apple_toolchain = false && '
+            '';
+          };
+        };
         gemset = import ./gemset.nix;
         bundixcli = bundix.packages.${system}.default;
 
@@ -41,14 +70,11 @@
         })
           env;
 
-        requirements = [ env ] ++ (with pkgs; [
-        ]);
+        requirements = [ env ];
 
         devShells = rec {
           default = dev;
-          dev = pkgs.mkShell {
-            buildInputs = requirements ++ [ bundixcli ];
-          };
+          dev = pkgs.mkShell { buildInputs = requirements ++ [ bundixcli ]; };
         };
       });
 }
