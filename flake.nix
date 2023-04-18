434d0e359cca7deed05a16b1dfd48f5a2e1f0ccc
Felix <zen9.felix@gmail.com>
2023-04-19 00:26:34 +0800

    Now, without postpatch

--- flake.nix
+++ flake.nix
@@ -49,17 +49,7 @@
             ];
             dontBuild = false;
             dontStrip = true;
-            postPatch = ''
-              substituteInPlace Makefile \
-                --replace '-Wno-invalid-source-encoding' ""
-            '' + lib.optionalString stdenv.isDarwin ''
-              # For < v1.48.0
-              substituteInPlace src/ruby/ext/grpc/extconf.rb \
-                --replace "ENV['AR'] = 'libtool -o' if RUBY_PLATFORM =~ /darwin/" ""
-              # For >= v1.48.0
-              substituteInPlace src/ruby/ext/grpc/extconf.rb \
-                --replace 'apple_toolchain = ' 'apple_toolchain = false && '
-            '';
+            postPatch = "";
           };
         };
         gemset = import ./gemset.nix;
