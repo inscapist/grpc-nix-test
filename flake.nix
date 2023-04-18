6ecaf1bbd94f64d023551c7a27048da18f35c3c9
Felix <zen9.felix@gmail.com>
2023-04-18 23:27:16 +0800

    Try removing postpatch content

--- flake.nix
+++ flake.nix
@@ -45,17 +45,7 @@
               "-Wno-error=stringop-truncation"
             ];
             dontBuild = false;
-            postPatch = ''
-              substituteInPlace Makefile \
-                --replace '-Wno-invalid-source-encoding' ""
-            '' + lib.optionalString stdenv.isDarwin ''
-              # For < v1.48.0
-              substituteInPlace src/ruby/ext/grpc/extconf.rb \
-                --replace "ENV['AR'] = 'libtool -o' if RUBY_PLATFORM =~ /darwin/" ""
-              # For >= v1.48.0
-              substituteInPlace src/ruby/ext/grpc/extconf.rb \
-                --replace 'apple_toolchain = ' 'apple_toolchain = false && '
-            '';
+            postPatch = "";
           };
         };
         gemset = import ./gemset.nix;
