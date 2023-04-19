b9e8dd00bb64d258f288995c9a4abd702559da6f
Felix <zen9.felix@gmail.com>
2023-04-19 23:42:06 +0800

    The N time is the charm

--- flake.nix
+++ flake.nix
@@ -31,7 +31,10 @@
         gemConfig = with pkgs; {
           grpc = attrs: {
             nativeBuildInputs = [ pkg-config ]
-              ++ lib.optional stdenv.isDarwin darwin.cctools;
+              ++ lib.optionals stdenv.isDarwin [
+                darwin.cctools
+                darwin.sig_tool
+              ];
             buildInputs = [ openssl ];
             hardeningDisable = [ "format" ];
             env.NIX_CFLAGS_COMPILE = toString [
@@ -49,19 +52,16 @@
               substituteInPlace Makefile \
                 --replace '-Wno-invalid-source-encoding' ""
             '' + lib.optionalString stdenv.isDarwin ''
-              # For < v1.48.0
-              substituteInPlace src/ruby/ext/grpc/extconf.rb \
-                --replace "ENV['AR'] = 'libtool -o' if RUBY_PLATFORM =~ /darwin/" ""
-              # For >= v1.48.0
+              # added by others, I am not wise enough to change it.
               substituteInPlace src/ruby/ext/grpc/extconf.rb \
                 --replace 'apple_toolchain = ' 'apple_toolchain = false && '
 
-              # address strip issue directly
-              # substituteInPlace src/ruby/ext/grpc/extconf.rb \
-              #   --replace "strip_tool += ' -x' if apple_toolchain" "strip_tool += ' -x'"
-
+              # don't strip binary
               substituteInPlace src/ruby/ext/grpc/extconf.rb \
-                --replace "if grpc_config == 'opt'" 'if false'
+                --replace 'hijack: all strip' 'hijack: all'
+            '';
+            postInstall = lib.optionalString stdenv.isDarwin ''
+              codesign --force --sign - $out/lib/ruby/gems/${ruby.gemPath}/gems/grpc-${attrs.version}/src/ruby/lib/grpc/grpc_c.bundle
             '';
           };
         };
